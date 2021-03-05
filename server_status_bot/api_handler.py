e24f2d531cc93c8f9cbee46ef41c182cba904368
John Thompson <jthomp@protonmail.com>
2021-03-05 07:21:59 -0600

    filter uneeded data

--- server_status_bot/api_handler.py
+++ server_status_bot/api_handler.py
@@ -7,10 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 URL = "https://api.battlemetrics.com/servers/9333822"
 TOKEN = os.getenv("BATTLE_METRICS_TOKEN")
-
+data = {}
 headers={"Authorization": "Bearer" + TOKEN}
 async def get_info():
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(URL) as resp:
             response = await resp.json()
-            return response
+            data["server_name"] = response["data"]["attributes"]["name"]
+            data["status"] = response["data"]["attributes"]["status"]
+            data["players_online"] = response["data"]["attributes"]["players"]
+            data["server_time"] = response["data"]["attributes"]["details"]["time"]
+            return data
