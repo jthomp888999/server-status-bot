aea4a6cbcd96b659afa4df9facd8539772aab061
John Thompson <jthomp@protonmail.com>
2021-03-05 08:09:04 -0600

    if called by name, answer with main

--- server_status_bot/bot.py
+++ server_status_bot/bot.py
@@ -33,5 +33,4 @@ async def send_status(ctx):
     embed.set_footer(text=f"There are currently {players_online} players online.")
     await ctx.send(embed=embed)
 
-if __name__ == "__main__":
-    bot.run(DISCORD_TOKEN)
+        bot.run(DISCORD_TOKEN)
